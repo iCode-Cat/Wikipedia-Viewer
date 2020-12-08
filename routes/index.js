9d73cb07d57093ed0699a60a660a136ffcb001b0
iCode-Cat <devranboyac8@gmail.com>
2020-12-08 22:59:35 +0200

    main

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,23 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({extended: true}))
 router.use(express.json());
-
-//Variables
 let value;
+//Variables
 
 router.get('/' , (req,res)=>{
+  
     res.render('index')
 })
 
 router.post('/' , (req,res)=>{
     value = req.body.data;
+    res.redirect("/query")
 })
-
-router.get('/query' , (req,res)=> {
+router.get('/query' , async (req,res)=> {
     const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${value}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => res.json(data['query']))
+  fetch(encodeURI(url))
+     .then(res => res.json())
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
 })
 module.exports = router;
